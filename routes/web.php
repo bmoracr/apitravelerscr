13135ac04f6801720ec6c93d410b6fa12d66e5fe
Bryan <bryanmora1994@gmail.com>
2021-12-30 17:31:59 -0600

    readme

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return "ok";
+    return view('welcome');
     
     // $encryptedValue = Crypt::encryptString('888888888');
 
