6a237524ffdc8ea00ad44904fdc9b6f9347e7bc4
Bryan <bryanmora1994@gmail.com>
2022-03-10 17:30:10 -0600

    ss

--- config/app.php
+++ config/app.php
@@ -1,9 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-return [
 
+return [
+    header('Access-Control-Allow-Origin: *'),
+    header('Access-Control-Allow-Methods: *'),
+    header('Access-Control-Allow-Headers: *'),
     /*
     |--------------------------------------------------------------------------
     | Application Name
