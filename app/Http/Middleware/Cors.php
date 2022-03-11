e9dab766cba8301c785fdb28126dc7767608d807
Bryan <bryanmora1994@gmail.com>
2022-03-10 18:32:42 -0600

    aa

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,8 +20,9 @@ class Cors
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Headers', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Authorization');
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Authorization')
+            ->header('allowed_methods', '*');
     }
 
     
